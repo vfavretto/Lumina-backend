13009c0098f9d41bbc88928512de1ab5a526b7e1
Zan-Kir <murilo4santos@hotmail.com>
2024-11-18 12:12:06 -0300

    updated dist

--- lumina_ts-main/dist/routes/admRoutes.js
+++ lumina_ts-main/dist/routes/admRoutes.js
@@ -5,10 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const admController_1 = require("../controllers/admController");
-const admController_2 = require("../controllers/admController");
 const router = express_1.default.Router();
+// Rotas protegidas - precisam do middleware de autenticação
+router.delete("/admin/:id", admController_1.verificarAutenticacaoAdmin, admController_1.deletarEmpresa);
+router.put("/admin/:id", admController_1.verificarAutenticacaoAdmin, admController_1.editarEmpresa);
+router.get("/admin", admController_1.verificarAutenticacaoAdmin, admController_1.listarEmpresas);
+router.get("/admin/:id", admController_1.verificarAutenticacaoAdmin, admController_1.buscarEmpresa);
 // Rota para validar login de administrador
 router.post("/admin/login", admController_1.validarAdmin);
-router.post("/admin/primeiro", admController_2.adicionarPrimeiroAdmin);
-router.post("/admin", admController_1.verificarAutenticacao, admController_2.adicionarAdmin);
+router.post("/admin/primeiro", admController_1.adicionarPrimeiroAdmin);
+router.post("/admin", admController_1.verificarAutenticacaoAdmin, admController_1.adicionarAdmin);
 exports.default = router;
